e0aab2c4ff0411b19fd3f0046ba8271ce7c50d2d
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-09 01:38:07 +0530

    delete and edit permission

--- src/pages/Permissions.tsx
+++ src/pages/Permissions.tsx
@@ -6,10 +6,13 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
   Grid2 as Grid,
   IconButton,
   InputAdornment,
+  Menu,
+  MenuItem,
   Table,
   TableBody,
   TableCell,
@@ -37,6 +40,13 @@ interface IPermissionLoaderData {
   }[];
 }
 
+interface IMoreMenuProps {
+  anchorEl: HTMLElement | null;
+  handleClose: () => void;
+  handleEditOpen: () => void;
+  handleDeletionOpen: () => void;
+}
+
 interface ICreatePermissionProps {
   open: boolean;
   body: { name: string; key: string; description: string };
@@ -46,6 +56,128 @@ interface ICreatePermissionProps {
   handleSubmit: () => Promise<void>;
   handleClose: () => void;
 }
+
+interface IDeletePermissionProps {
+  open: boolean;
+  loading: boolean;
+  handleClose: () => void;
+  handleDelete: () => void;
+}
+
+interface IEditPermissionProps {
+  open: boolean;
+  body: { name: string; key: string; description: string };
+  validation: { name: boolean; key: boolean };
+  loading: boolean;
+  handleInputChange: (name: string, value: string) => void;
+  handleSubmit: () => Promise<void>;
+  handleClose: () => void;
+}
+
+interface MoreOpenState {
+  open: HTMLElement | null;
+  state: {
+    id: string;
+    name: string;
+    key: string;
+    description: string;
+  };
+}
+
+const EditPermission: FC<IEditPermissionProps> = ({
+  open,
+  body,
+  validation,
+  loading,
+  handleInputChange,
+  handleSubmit,
+  handleClose,
+}) => {
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth>
+      <DialogTitle>Update Permission</DialogTitle>
+      <DialogContent>
+        <Grid container spacing={2} p={1} width="100%" direction="column">
+          <Grid>
+            <TextField
+              label="Name"
+              fullWidth
+              required
+              placeholder="e.g. System Access"
+              error={validation.name}
+              helperText={validation.name ? 'Must not be blank' : ''}
+              value={body.name}
+              onChange={(event) =>
+                handleInputChange('name', event.target.value)
+              }
+              slotProps={{
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+          </Grid>
+          <Grid>
+            <TextField
+              label="Key"
+              fullWidth
+              required
+              placeholder="feature:permission"
+              error={validation.key}
+              helperText={
+                validation.key
+                  ? "Key must follow the pattern '[segment1]:[segment2]'"
+                  : ''
+              }
+              value={body.key}
+              onChange={(event) => handleInputChange('key', event.target.value)}
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">org:</InputAdornment>
+                  ),
+                },
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+          </Grid>
+          <Grid>
+            <TextField
+              label="Description"
+              fullWidth
+              placeholder="Permission to access all system resources"
+              value={body.description}
+              multiline
+              rows={3}
+              onChange={(event) =>
+                handleInputChange('description', event.target.value)
+              }
+              slotProps={{
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="inherit">
+          Cancel
+        </Button>
+        <LoadingButton
+          variant="contained"
+          loading={loading}
+          onClick={handleSubmit}
+        >
+          Update
+        </LoadingButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 const CreatePermission: FC<ICreatePermissionProps> = ({
   open,
@@ -142,12 +274,66 @@ const CreatePermission: FC<ICreatePermissionProps> = ({
   );
 };
 
+const DeletionModal: FC<IDeletePermissionProps> = ({
+  open,
+  loading,
+  handleClose,
+  handleDelete,
+}) => {
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth>
+      <DialogTitle sx={{ m: 0, p: 2 }}>Delete application?</DialogTitle>
+      <DialogContent>
+        <DialogContentText gutterBottom>
+          Are you sure you want to delete this permission? This is irreversible
+          and all roles with this permission will be unlinked.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="inherit">
+          Cancel
+        </Button>
+        <LoadingButton
+          loading={loading}
+          color="error"
+          variant="contained"
+          onClick={handleDelete}
+        >
+          Delete
+        </LoadingButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const MoreMenu: FC<IMoreMenuProps> = ({
+  anchorEl,
+  handleEditOpen,
+  handleDeletionOpen,
+  handleClose,
+}) => {
+  return (
+    <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={handleClose}>
+      <MenuItem onClick={handleEditOpen}>Edit</MenuItem>
+      <MenuItem sx={{ color: 'error.main' }} onClick={handleDeletionOpen}>
+        Delete
+      </MenuItem>
+    </Menu>
+  );
+};
+
 const Permissions = () => {
   const [addPermission, setAddPermission] = useState(false);
+  const [editPermission, setEditPermission] = useState(false);
+  const [deletionOpen, setDeletionOpen] = useState(false);
   const [body, setBody] = useState({
     name: '',
     key: '',
     description: '',
+  });
+  const [moreMenuOpen, setMoreMenuOpen] = useState<MoreOpenState>({
+    open: null,
+    state: { id: '', name: '', key: '', description: '' },
   });
   const [apiResponse, setApiResponse] = useState({
     error: false,
@@ -246,14 +432,126 @@ const Permissions = () => {
     }
   };
 
+  const handleEditPermission = async (id: string) => {
+    const tempValidation = { ...validation };
+    let validationCount = 0;
+    if (body.name.length < 1) {
+      tempValidation.name = true;
+      validationCount++;
+    }
+    if (!/^[a-z0-9_]+:[a-z0-9_]+$/.test(body.key)) {
+      tempValidation.key = true;
+      validationCount++;
+    }
+    if (validationCount > 0) {
+      setValidation(tempValidation);
+    } else {
+      setApiResponse({ ...apiResponse, loading: true });
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/permission/update/${id}`,
+          {
+            method: 'PUT',
+            credentials: 'include',
+            body: JSON.stringify(body),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+        if (response.ok) {
+          setApiResponse({
+            ...apiResponse,
+            success: true,
+            error: false,
+            message: 'Updated permission',
+          });
+        } else {
+          constants.fetchError(response.status);
+        }
+      } catch (error: any) {
+        setApiResponse({
+          ...apiResponse,
+          error: true,
+          success: false,
+          message: error.message || 'Error updating permission',
+        });
+      }
+    }
+  };
+
+  const handleDeletePermission = async (id: string) => {
+    setApiResponse({ ...apiResponse, loading: true });
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/permission/delete/${id}`,
+        {
+          method: 'DELETE',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      if (response.ok) {
+        setApiResponse({
+          ...apiResponse,
+          success: true,
+          error: false,
+          message: 'Deleted permission',
+        });
+        setDeletionOpen(false);
+        handleMoreMenuClose();
+        revalidate();
+      } else {
+        constants.fetchError(response.status);
+      }
+    } catch (error: any) {
+      setApiResponse({
+        ...apiResponse,
+        success: false,
+        error: true,
+        message: error.message || 'Error deleting permission',
+      });
+    }
+  };
+
+  const handleEditPermissionModalOpen = () => {
+    setBody({
+      ...body,
+      name: moreMenuOpen.state.name,
+      key: moreMenuOpen.state.key.replace('org:', ''),
+      description: moreMenuOpen.state.description,
+    });
+    setEditPermission(true);
+    handleMoreMenuClose();
+  };
+
+  const handleDeletionModalOpen = () => {
+    setDeletionOpen(true);
+  };
+
   const handleInputChange = (name: string, value: string) => {
     setValidation({ ...validation, [name]: false });
     setBody({ ...body, [name]: value });
   };
 
-  const handlePermissionModalClose = () => {
+  const handleMoreMenuClose = () => {
+    setMoreMenuOpen({ ...moreMenuOpen, open: null });
+  };
+
+  const handleEditPermissionModalClose = () => {
+    setBody({ ...body, name: '', key: '', description: '' });
+    setEditPermission(false);
+  };
+
+  const handleCreatePermissionModalClose = () => {
     setBody({ ...body, name: '', key: '', description: '' });
     setAddPermission(false);
+  };
+
+  const handleDeletionModalClose = () => {
+    setDeletionOpen(false);
   };
 
   return (
@@ -270,17 +568,40 @@ const Permissions = () => {
             error: false,
           });
           revalidate();
-          handlePermissionModalClose();
+          handleCreatePermissionModalClose();
+          handleEditPermissionModalClose();
+          handleDeletionModalClose();
         }}
+      />
+      <MoreMenu
+        anchorEl={moreMenuOpen.open}
+        handleEditOpen={handleEditPermissionModalOpen}
+        handleDeletionOpen={handleDeletionModalOpen}
+        handleClose={handleMoreMenuClose}
       />
       <CreatePermission
         open={addPermission}
         body={body}
         validation={validation}
         loading={apiResponse.loading}
-        handleClose={handlePermissionModalClose}
+        handleClose={handleCreatePermissionModalClose}
         handleInputChange={handleInputChange}
         handleSubmit={handleCreatePermission}
+      />
+      <EditPermission
+        open={editPermission}
+        body={body}
+        validation={validation}
+        loading={apiResponse.loading}
+        handleClose={handleEditPermissionModalClose}
+        handleInputChange={handleInputChange}
+        handleSubmit={() => handleEditPermission(moreMenuOpen.state.id)}
+      />
+      <DeletionModal
+        open={deletionOpen}
+        loading={apiResponse.loading}
+        handleClose={handleDeletionModalClose}
+        handleDelete={() => handleDeletePermission(moreMenuOpen.state.id)}
       />
       <Grid container width="100%" spacing={2} direction="column">
         <Grid container width="100%" justifyContent="space-between">
@@ -327,7 +648,20 @@ const Permissions = () => {
                     )}`}</TableCell>
                     <TableCell>
                       <GeneralTooltip title="More Info" arrow>
-                        <IconButton>
+                        <IconButton
+                          onClick={(event) =>
+                            setMoreMenuOpen({
+                              ...moreMenuOpen,
+                              open: event.currentTarget,
+                              state: {
+                                id: value.id,
+                                key: value.key,
+                                name: value.name,
+                                description: value.description,
+                              },
+                            })
+                          }
+                        >
                           <MoreHoriz />
                         </IconButton>
                       </GeneralTooltip>
