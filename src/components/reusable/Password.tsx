f5ca9cc9aca7be017e86545aa6a8336be396d956
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-18 23:49:43 +0530

    placeholder props added

--- src/components/reusable/Password.tsx
+++ src/components/reusable/Password.tsx
@@ -1,5 +1,6 @@
 import { Close, Done, Visibility, VisibilityOff } from '@mui/icons-material';
 import {
+  Grid2 as Grid,
   IconButton,
   Stack,
   TextField,
@@ -7,140 +8,170 @@ import {
   Typography,
 } from '@mui/material';
 import { FC, useState } from 'react';
+import constants from '../../config/constants';
 
-type PasswordProps = Omit<TextFieldProps, 'onChange' | 'error' | 'type'> &
-  Required<{ onChange: (value: string) => void }>;
+type PasswordProps = Omit<
+  TextFieldProps,
+  'onChange' | 'error' | 'type' | 'placeholder'
+> &
+  Required<{
+    onChange: (value: string) => void;
+    placeholder: string;
+    confirmPasswordPlaceholder: string;
+  }>;
 
 const Password: FC<PasswordProps> = ({
   label,
+  placeholder,
+  confirmPasswordPlaceholder,
   autoComplete,
   slotProps,
   onChange,
   ...otherProps
 }) => {
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [visible, setVisible] = useState(false);
 
   const uppercaseRegex = /[A-Z]/;
   const lowercaseRegex = /[a-z]/;
-  const specialCharRegex = /[@#!$%^&]/;
+  const specialCharRegex = /[@#!$%^&-]/;
   const numberRegex = /\d/;
   const lengthRegex = /.{8,}/;
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[@#!$%^&])(?=.*\d).{8,}$/;
+  const confirmPasswordError =
+    confirmPassword.length !== 0 && password !== confirmPassword;
 
   return (
-    <TextField
-      {...otherProps}
-      label={label || 'Password'}
-      value={password}
-      type={visible ? 'text' : 'password'}
-      onChange={(event) => {
-        setPassword(event.target.value);
-        onChange(event.target.value);
-      }}
-      error={!passwordRegex.test(password) && password.length > 0}
-      autoComplete={autoComplete || 'new-password'}
-      slotProps={{
-        ...slotProps,
-        input: {
-          ...slotProps?.input,
-          endAdornment: (
-            <IconButton onClick={() => setVisible(!visible)}>
-              {visible ? <Visibility /> : <VisibilityOff />}
-            </IconButton>
-          ),
-        },
-      }}
-      helperText={
-        <Stack spacing={1}>
-          {uppercaseRegex.test(password) ? (
-            <Typography
-              color="success"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Done fontSize="inherit" color="inherit" />
-              Atleast one uppercase character
-            </Typography>
-          ) : (
-            <Typography
-              color="error"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Close fontSize="inherit" color="inherit" />
-              Atleast one uppercase character
-            </Typography>
-          )}
-          {lowercaseRegex.test(password) ? (
-            <Typography
-              color="success"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Done fontSize="inherit" color="inherit" />
-              Atleast one lowercase character
-            </Typography>
-          ) : (
-            <Typography
-              color="error"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Close fontSize="inherit" color="inherit" />
-              Atleast one lowercase character
-            </Typography>
-          )}
-          {specialCharRegex.test(password) ? (
-            <Typography
-              color="success"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Done fontSize="inherit" color="inherit" />
-              Atleast one special character ({`@,#,!,$,%,^,&`})
-            </Typography>
-          ) : (
-            <Typography
-              color="error"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Close fontSize="inherit" color="inherit" />
-              Atleast one special character ({`@,#,!,$,%,^,&`})
-            </Typography>
-          )}
-          {numberRegex.test(password) ? (
-            <Typography
-              color="success"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Done fontSize="inherit" color="inherit" />
-              Atleast one digit
-            </Typography>
-          ) : (
-            <Typography
-              color="error"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Close fontSize="inherit" color="inherit" />
-              Atleast one digit
-            </Typography>
-          )}
-          {lengthRegex.test(password) ? (
-            <Typography
-              color="success"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Done fontSize="inherit" color="inherit" />
-              Minimum 8 character length
-            </Typography>
-          ) : (
-            <Typography
-              color="error"
-              sx={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Close fontSize="inherit" color="inherit" />
-              Minimum 8 character length
-            </Typography>
-          )}
-        </Stack>
-      }
-    />
+    <Grid container spacing={1}>
+      <Grid width="100%">
+        <TextField
+          {...otherProps}
+          label={label || 'Password'}
+          value={password}
+          type={visible ? 'text' : 'password'}
+          placeholder={placeholder}
+          onChange={(event) => {
+            setPassword(event.target.value);
+            onChange(!confirmPasswordError ? event.target.value : '');
+          }}
+          error={!constants.passwordRegex.test(password) && password.length > 0}
+          autoComplete={autoComplete || 'new-password'}
+          slotProps={{
+            ...slotProps,
+            input: {
+              ...slotProps?.input,
+              endAdornment: (
+                <IconButton onClick={() => setVisible(!visible)}>
+                  {visible ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              ),
+            },
+          }}
+          helperText={
+            <Stack spacing={1}>
+              {uppercaseRegex.test(password) ? (
+                <Typography
+                  color="success"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Done fontSize="inherit" color="inherit" />
+                  Atleast one uppercase character
+                </Typography>
+              ) : (
+                <Typography
+                  color="error"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Close fontSize="inherit" color="inherit" />
+                  Atleast one uppercase character
+                </Typography>
+              )}
+              {lowercaseRegex.test(password) ? (
+                <Typography
+                  color="success"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Done fontSize="inherit" color="inherit" />
+                  Atleast one lowercase character
+                </Typography>
+              ) : (
+                <Typography
+                  color="error"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Close fontSize="inherit" color="inherit" />
+                  Atleast one lowercase character
+                </Typography>
+              )}
+              {specialCharRegex.test(password) ? (
+                <Typography
+                  color="success"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Done fontSize="inherit" color="inherit" />
+                  Atleast one special character ({`@,#,!,$,%,^,&`})
+                </Typography>
+              ) : (
+                <Typography
+                  color="error"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Close fontSize="inherit" color="inherit" />
+                  Atleast one special character ({`@,#,!,$,%,^,&,-`})
+                </Typography>
+              )}
+              {numberRegex.test(password) ? (
+                <Typography
+                  color="success"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Done fontSize="inherit" color="inherit" />
+                  Atleast one digit
+                </Typography>
+              ) : (
+                <Typography
+                  color="error"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Close fontSize="inherit" color="inherit" />
+                  Atleast one digit
+                </Typography>
+              )}
+              {lengthRegex.test(password) ? (
+                <Typography
+                  color="success"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Done fontSize="inherit" color="inherit" />
+                  Minimum 8 character length
+                </Typography>
+              ) : (
+                <Typography
+                  color="error"
+                  sx={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Close fontSize="inherit" color="inherit" />
+                  Minimum 8 character length
+                </Typography>
+              )}
+            </Stack>
+          }
+        />
+      </Grid>
+      <Grid width="100%">
+        <TextField
+          type="password"
+          label="Confirm Password"
+          placeholder={confirmPasswordPlaceholder}
+          error={confirmPasswordError}
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          helperText={confirmPasswordError ? 'Password does not match' : null}
+          fullWidth
+          slotProps={slotProps}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
