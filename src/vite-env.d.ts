968c37f741d0be2e8cb40c9b26dda417004f44ad
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-02-08 00:19:05 +0530

    Docs url added

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_APP_URL: string;
   readonly VITE_BASE_URL: string;
+  readonly VITE_DOCS_URL: string;
   readonly VITE_RAZORPAY_KEY: string;
 }
 
